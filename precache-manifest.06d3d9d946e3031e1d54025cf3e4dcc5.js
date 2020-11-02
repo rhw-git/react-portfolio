3d8b2bd961334176eb3d193b223258a9fdbe2587
Ruohan Wang <ruohanw89@gmail.com>
2020-11-02 00:57:54 -0800

    Updates

---
+++ precache-manifest.06d3d9d946e3031e1d54025cf3e4dcc5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb9f2d4828fbe8e3a37c05914c0633d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b7dc894d0cffd2d458c2",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5cf76eda85caaa692a25",
+    "url": "/react-portfolio/static/css/main.ed42c06f.chunk.css"
+  },
+  {
+    "revision": "b7dc894d0cffd2d458c2",
+    "url": "/react-portfolio/static/js/2.6fa377ea.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/react-portfolio/static/js/2.6fa377ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf76eda85caaa692a25",
+    "url": "/react-portfolio/static/js/main.68c1f4f5.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "3ea68a25eb76d2b15ac0be0782c93cea",
+    "url": "/react-portfolio/static/media/budget-tracker.3ea68a25.png"
+  },
+  {
+    "revision": "29a4f0caec24fc3d924d0224158cb764",
+    "url": "/react-portfolio/static/media/code-quiz.29a4f0ca.png"
+  },
+  {
+    "revision": "00448dc1dafc2f596b99cb5f4178fe63",
+    "url": "/react-portfolio/static/media/cosmopolitan-tastes.00448dc1.png"
+  },
+  {
+    "revision": "9a5120047bed62d1806ebb7f4cea8035",
+    "url": "/react-portfolio/static/media/jumbo-image.9a512004.jpg"
+  },
+  {
+    "revision": "c0f05053c869d6b0140495d340c51c89",
+    "url": "/react-portfolio/static/media/profile-img.c0f05053.png"
+  },
+  {
+    "revision": "c778cf712742dfa6c2dc3d3b3bcf244d",
+    "url": "/react-portfolio/static/media/tech-blog.c778cf71.png"
+  },
+  {
+    "revision": "5dfb68194f7d0c31239cbaf71a0bcb9e",
+    "url": "/react-portfolio/static/media/weather-dashboard.5dfb6819.png"
+  },
+  {
+    "revision": "5e1e047bbc3805aa347175f37c322f37",
+    "url": "/react-portfolio/static/media/whats-in-your-fridge.5e1e047b.png"
+  }
+]);
